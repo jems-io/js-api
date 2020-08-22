cf3fcb287d9b3c91d2a64cf8b12500ce3e2b5dc3
Francico Mercedes <franciscomerdot@gmail.com>
2020-08-21 22:10:02 -0400

    Remove middlewares and routines from ApiProtected and set it to readonly

--- packages/domain/models/api-protected.ts
+++ packages/domain/models/api-protected.ts
@@ -1,10 +1,14 @@
+import { Api } from "./api";
 import { ApiResource } from "./api-resource";
-import { ApiResourceActionMiddleware } from "./api-resource-action-middleware";
+import { ApiResourceAction } from "./api-resource-action";
+import { ApiResourceEvent } from "./api-resource-event";
 
-export interface ApiProtected { // TODO: Remove routines
-    name: string
-    version: string
-    description?: string
-    resources: ApiResource[]
-    resourcesActionsMiddlewares?: ApiResourceActionMiddleware[]
+interface ApiResourceProtected extends Readonly<Omit<ApiResource, 'actions' | 'events' | 'resources' | 'actionsMiddlewares'>> {
+    readonly actions: Array<Readonly<Omit<ApiResourceAction, 'routine' | 'middlewares'> & { id: string }>>
+    readonly events: Array<Readonly<ApiResourceEvent & { id: string }>>
+    readonly resources: ApiResourceProtected[], 
+}
+
+export interface ApiProtected extends Readonly<Omit<Api, 'resources' | 'resourcesActionsMiddlewares'>> {
+    readonly resources: ApiResourceProtected[] 
 }
