aafb343cc4d5a5a16da6f23b41f4a73eb246adab
Francico Mercedes <franciscomerdot@gmail.com>
2020-08-21 22:15:31 -0400

    Subscribe to events at ResourceEventPipelineSer..

--- packages/domain/services/resource-event-pipeline-service.ts
+++ packages/domain/services/resource-event-pipeline-service.ts
@@ -1,7 +1,8 @@
 
-import { ApiResponse } from "../models/api-respose";
 import { ApiEvent } from "../models/api-event";
 
 export interface ResourceEventPipelineService {
     pipe(eventId: string, eventContent: ApiEvent): Promise<void>
+    subscribeListener(eventId: string, handler: (event: ApiEvent) => Promise<void>): Promise<void>
+    unsubscribeListener(eventId: string, handler: (event: ApiEvent) => Promise<void>): Promise<void>
 }
