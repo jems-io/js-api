f0f6ed1808d4ac4aaaef33275e6d017390f3f747
Francico Mercedes <franciscomerdot@gmail.com>
2021-06-10 01:25:38 -0400

    Add couple cases to the sandbox

--- packages/sandbox/index.ts
+++ packages/sandbox/index.ts
@@ -1,17 +1,75 @@
 import {
-    createApiRuntime
+  createApiResponseBuildService,
+  createApiRuntime,
 } from "@jems/api-core";
 import { ExpressActionDeliveryService } from "@jems/api-delivery-http-express";
 import { Api } from "@jems/api-domain";
 
-
+const apiResponseBuildService = createApiResponseBuildService();
 const api: Api = {
   name: "Sanbox Api",
   version: "1.0",
   resources: [
     {
-      alias: "resource",
-      name: "Resource",
+      alias: "users",
+      name: "Users",
+      actions: [
+        {
+          type: "query",
+          name: "Query Users",
+          routine: (req) => apiResponseBuildService.buildJsonResponse(req),
+        },
+        {
+          type: "get",
+          name: "Get User",
+          routine: (req) =>
+            apiResponseBuildService.buildJsonResponse(
+              { message: "Resource Not Found" },
+              "resourceNotFound"
+            ),
+        },
+        {
+          type: "get",
+          alias: "default2",
+          name: "Get Users",
+          routine: (req) =>
+            apiResponseBuildService.buildJsonResponse(
+              { message: "Resource Not Found 2" },
+              "resourceNotFound"
+            ),
+        },
+      ],
+    },
+    {
+      alias: "friends",
+      name: "Friends",
+      actions: [
+        {
+          type: "query",
+          name: "Query Friends",
+          routine: (req) => apiResponseBuildService.buildJsonResponse(req),
+        },
+        {
+          type: "get",
+          name: "Get Friend",
+          routine: (req) => {
+            return apiResponseBuildService.buildJsonResponse(
+              { message: "Resource Not Found" },
+              "resourceNotFound"
+            );
+          },
+        },
+        {
+          type: "get",
+          alias: "default2",
+          name: "Get Friends",
+          routine: (req) =>
+            apiResponseBuildService.buildJsonResponse(
+              { message: "Resource Not Found 2" },
+              "resourceNotFound"
+            ),
+        },
+      ],
     },
   ],
 };
